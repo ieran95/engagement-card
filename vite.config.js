083b7dcbe3facfd1d39bec57741b411b6d50cb7a
Shah <shahiran.bh@gmail.com>
2026-09-12 16:14:18 +0800

    chore: rollback

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,3 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-    plugins: [vue()],
-    base: '/engagement-card/'
-})
+export default defineConfig({ plugins:[vue()], base:'./' })
