1021bc0e0e43b864923d22d8468da4e212848068
Shah <shahiran.bh@gmail.com>
2026-09-12 16:12:02 +0800

    fix: update base

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-export default defineConfig({ plugins:[vue()], base:'./' })
+
+export default defineConfig({
+    plugins: [vue()],
+    base: '/engagement-card/'
+})
